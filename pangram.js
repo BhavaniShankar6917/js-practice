ce8437bb047e93bac93270de5f910921468e219a
BhavaniShankar6917 <91416094+BhavaniShankar6917@users.noreply.github.com>
2023-01-15 08:04:35 +0530

    Create pangram.js

---
+++ pangram.js
@@ -0,0 +1,16 @@
+// pangram is a sentence that contains all the letters in the english alphabets atleast once.
+// This program checks to see if a given sentence is a pangram or not.
+
+function isPangram(sentence){
+    const alph = "abcdefghijklmnopqrstuvwxyz";
+    let newSentence = sentence.toLowerCase().replaceAll(" ", "");
+    console.log(newSentence);
+    let result = true;
+    for (let letter of alph)
+        if (!newSentence.includes(letter))  
+            return false;
+        result = true;
+    return result;
+}
+console.log(isPangram("Fox dwarves chop my talking quiz job"));
+console.log(isPangram("Not a pangram"));
