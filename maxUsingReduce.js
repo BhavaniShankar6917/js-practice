4f089c074b90cce069046074dacafbd95fa19b01
BhavaniShankar6917 <91416094+BhavaniShankar6917@users.noreply.github.com>
2023-01-16 12:25:02 +0530

    Update maxUsingReduce.js

--- maxUsingReduce.js
+++ maxUsingReduce.js
@@ -5,3 +5,23 @@ const max = marks.reduce((initial, current) => {  //initial = 85, current = 86
     return initial;                               //else initial = initial, current becomes the next value.
 });
 console.log(max);
+//
+//
+//
+/************condensed version*******************/
+//
+//
+//
+const marks = [85,86,88,80,90,82,87];
+const max = marks.reduce((initial, current) => Math.max(initial,current));
+console.log(max);
+//
+//
+//
+/************min version*******************/
+//
+//
+//
+const marks = [85,86,88,80,90,82,87];
+const min = marks.reduce((initial, current) => Math.min(initial,current));
+console.log(min);
