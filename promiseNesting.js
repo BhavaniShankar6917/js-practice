db6081f96ac065a218a0e8732383b1402d5a9ac2
BhavaniShankar6917 <91416094+BhavaniShankar6917@users.noreply.github.com>
2023-02-06 20:32:35 +0530

    Create promiseNesting.js

---
+++ promiseNesting.js
@@ -0,0 +1,44 @@
+const request = (url) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const rand = Math.random();
+      const pages = {
+        "/users": [
+          { id: 1, username: "Matthew" },
+          { id: 2, username: "alberto" },
+        ],
+        "/users/1": {
+          name: "Matthew",
+          id: 1,
+          born: 1996,
+          country: "Austria",
+        },
+        "/users/1/Austria": {
+          birthPlace: "Vienna",
+          dob: "12-12-1996",
+        },
+        "/about": "This is the about page",
+      };
+      const data = pages[url];
+      if (data) {
+        resolve({ status: 200, data });
+      } else {
+        reject({ status: 404 });
+      }
+    }, 1000);
+  });
+};
+request("/users")
+  .then((res) => {
+    const userid = res.data[0].id;
+    request(`/users/${userid}`).then((res) => {
+      const country = res.data.country;
+      request(`/users/${userid}/${country}`).then((res) => {
+        console.log(res.data);
+        // console.log(userid);
+      });
+    });
+  })
+  .catch((res) => {
+    console.log("status", res.status);
+  });
