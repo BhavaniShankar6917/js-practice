2f562917689caf4bda6488784670c3f81abe261b
BhavaniShankar6917 <91416094+BhavaniShankar6917@users.noreply.github.com>
2023-02-07 12:06:52 +0530

    Update promise.js

--- promise.js
+++ promise.js
@@ -24,3 +24,28 @@ request("/about").then((res) => {               ///Here the url passed is '/abou
 request().catch((res) => {
   console.log("status", res.status);
 });
+
+//*************////
+const button = document.querySelector("button");
+const initButtonRect = button.getBoundingClientRect();
+const moveX = (amount, delay) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const bodyBoundary = document.body.clientWidth;
+      const elRight = button.getBoundingClientRect().right;
+      const currLeft = initButtonRect.left;
+      if (elRight + amount > bodyBoundary) {
+        reject();
+      } else {
+        button.style.transform = `translateX(${elRight - currLeft + amount}px)`;
+        resolve();
+      }
+    }, delay);
+  });
+};
+moveX(100, 1000)
+  .then(() => moveX(100, 1000))
+  .then(() => moveX(100, 1000))
+  .then(() => moveX(100, 1000))
+  .then(() => moveX(100, 1000))
+  .catch(() => console.log("CANNOT MOVE FURTHER"));
